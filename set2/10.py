8be0b7227e3a365c2ae12db0ac247d5ea1aa6daf
beard33 <andreaguzzon26@gmail.com>
2020-12-27 23:17:22 +0100

    Clean set2 code

--- set2/10.py
+++ set2/10.py
@@ -3,39 +3,14 @@ import utils.tools as tools
 from base64 import b64decode, b64encode
 from Crypto.Cipher import AES
 
-BLOCK_SIZE = 16
+test = b'YellowSubmarine'
+key = b'YELLOW SUBMARINE'
 
-def cbcEncrypt(plaintext, key, IV = b'\x00'*BLOCK_SIZE):
-    assert len(key) == 16 and len(IV) == 16, "Key and IV must be 16 bytes long"
-    ciphertext = []
-    cipher = AES.new(key, AES.MODE_ECB)
-    paddedPt = tools.pkcs7Padding(plaintext, BLOCK_SIZE)
-    previousBlock = cipher.encrypt(tools.singleXOR(IV, paddedPt[ :BLOCK_SIZE]))
-    ciphertext += previousBlock
-    for i in range(BLOCK_SIZE,len(plaintext), BLOCK_SIZE):
-        block = cipher.encrypt(tools.singleXOR(previousBlock, paddedPt[i : i+BLOCK_SIZE]))
-        previousBlock = block
-        ciphertext += block
-
-    return bytes(ciphertext)
-
-
-def cbcDecrypt(ciphertext, key, IV = b'\x00'*BLOCK_SIZE):
-    assert len(key) == 16 and len(IV) == 16, "Key and IV must be 16 bytes long"
-    plaintext = []
-    cipher = AES.new(key, AES.MODE_ECB)
-    for i in range(len(ciphertext), BLOCK_SIZE, -BLOCK_SIZE):
-        block = cipher.decrypt(ciphertext[i-BLOCK_SIZE : i])
-        plaintext.insert(0, tools.singleXOR(block, ciphertext[i-2*BLOCK_SIZE : i - BLOCK_SIZE]))
-
-    plaintext.insert(0, tools.singleXOR(cipher.decrypt(ciphertext[:BLOCK_SIZE]), IV))
-
-    return b''.join(plaintext)
+# Testing if encryption and decryption processes work on test string and on given file
+print(tools.cbcDecrypt(tools.cbcEncrypt(tools.pkcs7Padding(test, tools.BLOCK_SIZE), key), key))
+print()
 
 with open("10/10.txt", "r") as f:
     text = b64decode(f.read())
-key = b'YELLOW SUBMARINE'
-
-
-plaintext = cbcDecrypt(text, key)
+plaintext = tools.cbcDecrypt(text, key)
 print(bytes(plaintext).decode("utf-8"))
--- set2/9.py
+++ set2/9.py
@@ -3,7 +3,6 @@ import utils.tools as tools
 string = b'YELLOW SUBMARINE'
 target = b'YELLOW SUBMARINE\x04\x04\x04\x04'
 
-
 if target == tools.pkcs7Padding(string, 20):
     print("OK")
 else:
